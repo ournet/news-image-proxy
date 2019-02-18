e226ca333dda113e83453e1573835898da15afd0
Mitica <canteadumitru@gmail.com>
2019-02-18 11:32:55 +0200

    image src fixes

--- src/quotes/formatters/v1.ts
+++ src/quotes/formatters/v1.ts
@@ -4,6 +4,7 @@ import { Quote } from "@ournet/api-client";
 import moment = require("moment");
 import { truncateText } from "../../helpers";
 import { getHost } from 'ournet.links';
+import { ImageStorageHelper } from "@ournet/images-domain";
 
 export default (quote: Quote) => {
     const backColor = '#f2f0ea';
@@ -14,16 +15,23 @@ export default (quote: Quote) => {
     const quoteTextColor = '#f3f3f3';
     const muteColor = '#999';
     const width = 640;
-    const headerHeight = 110;
+    const headerHeight = 100;
     // const height = width * (0.562);
     // const maxQuoteTextHeight = height/10;
 
+    let imageSrc = '';
     let imageId: string = '';
     if (quote.events && quote.events.length) {
         imageId = quote.events[quote.events.length - 1].imageId || imageId;
+        if (imageId) {
+            imageSrc = 'http:' + ImageStorageHelper.eventUrl(imageId, 'large');
+        }
     }
     if (!imageId) {
         imageId = quote.source.imageId || imageId;
+        if (imageId) {
+            imageSrc = 'http:' + ImageStorageHelper.newsUrl(imageId, 'large');
+        }
     }
 
     const date = moment(quote.lastFoundAt).locale(quote.lang);
@@ -32,7 +40,7 @@ export default (quote: Quote) => {
 
     const ournetLink = getHost('news', quote.country);
 
-    const newsText = quote.events && quote.events.length && quote.events[quote.events.length-1].title || quote.source.title;
+    const newsText = quote.events && quote.events.length && quote.events[quote.events.length - 1].title || quote.source.title;
 
     return `
     <html>
@@ -78,9 +86,12 @@ export default (quote: Quote) => {
             width: 50px;
             height: 50px;
         }
-        #quote {
-            margin-bottom: 25px;
-            margin-top: 6px;
+        #header {
+            height: ${headerHeight-25}px;
+            position: relative;
+        }
+        #body {
+            min-height: 200px;
         }
         #quote-text {
             background: rgba(0,0,0,.9);
@@ -97,6 +108,7 @@ export default (quote: Quote) => {
             text-indent: 22px;
             border-radius: 3px;
             text-shadow: 0px 1px 1px #000;
+            margin-bottom: 25px;
         }
         .with-image #quote-text {
             background: rgba(0,0,0,.4);
@@ -110,11 +122,7 @@ export default (quote: Quote) => {
             font-weight: 700;
             text-indent: 0;
         }
-        #quote-media {
-            padding-bottom: 25px;
-            position: relative;
-        }
-        #quote-media:before {
+        #header:before {
             content: "";
             width: 0;
             height: 0;
@@ -166,8 +174,8 @@ export default (quote: Quote) => {
         .with-image #news {
             color: #fff;
         }
-        .with-image>#back-image {
-            background-image: url('http://news.ournetcdn.net/events/${imageId.substr(0, 3)}/master/${imageId}.jpg');
+        .with-image #back-image {
+            background-image: url('${imageSrc}');
             background-size: cover;
             background-repeat: no-repeat;
             background-position-x: center;
@@ -200,29 +208,29 @@ export default (quote: Quote) => {
         <body>
         <div id="e1"></div>
         <div id="e2"></div>
-        <div id="container" class="${imageId ? 'with-image' : ''}">
+        <div id="container" class="${imageId.length > 0 ? 'with-image' : ''}">
             <div id="back-image"></div>
             <div id="back-image-mask"></div>
             <div id="logo">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3937 3937"><g><path fill="#2b363c" fill-rule="nonzero" d="M668 1298c25-23 27-61 4-86L466 984c-23-25-61-27-86-4l-228 206c-25 23-27 61-4 87l206 228c23 25 62 27 86 4l229-206zM896 679c25 7 51-7 58-32l65-224c7-24-7-51-32-58l-224-65c-25-7-50 7-58 32l-65 224c-7 25 7 50 32 58l224 65zM1204 384c-4 24 12 47 36 51l221 35c24 4 47-12 51-37l36-221c3-24-13-47-37-51l-220-36c-24-4-47 13-51 37l-35 220zM1004 1334c-31 14-67 1-81-30l-127-279c-14-31-1-67 29-81l279-128c31-14 67 0 81 30l127 279c14 30 0 67-30 81l-279 127zM1018 2345c-54 26-118 2-143-51l-232-489c-25-55-3-118 51-144l490-232c54-25 118-2 143 51l232 490c26 54 3 118-51 143l-490 232z"></path><path fill="#ca0000" fill-rule="nonzero" d="M2978 788c-494-269-1069-245-1527 9l339 724c235-154 544-177 808-34 382 208 523 688 315 1069-208 382-688 523-1069 315-187-102-316-269-375-458l-216 101c-16 9-33 16-50 23l-459 214c130 338 376 633 718 819 768 418 1732 133 2150-635s133-1732-635-2149z"></path></g></svg>
             </div>
-            <div id="quote">
-                <div id="quote-media">
-                    <img id="quote-media__icon" src="${authorImage}" alt=${quote.author.name}/>
-                    <div id="quote-media__body">
-                        <div id="author-name">${quote.author.name}</div>
-                        <div id="quote-date">${date.format('LL')}</div>
-                    </div>
+            <div id="header">
+                <img id="quote-media__icon" src="${authorImage}" alt=${quote.author.name}/>
+                <div id="quote-media__body">
+                    <div id="author-name">${quote.author.name}</div>
+                    <div id="quote-date">${date.format('LL')}</div>
                 </div>
+            </div>
+            <div id="body">
                 <div id="quote-text">
                     <i>“</i>
                     ${truncateText(quote.text, 280)}
                 </div>
+                <div id="news">
+                    <span>—</span> ${truncateText(newsText, 120)}
+                </div>
+                <a id='ournet-link'>${ournetLink}</a>
             </div>
-            <div id="news">
-                <span>—</span> ${truncateText(newsText, 120)}
-            </div>
-            <a id='ournet-link'>${ournetLink}</a>
         </div>
         </body>
     </html>`;
