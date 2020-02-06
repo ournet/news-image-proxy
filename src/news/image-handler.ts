290afdbf7054ad5e97a34665d11249f789470b96
Dumitru C <canteadumitru@gmail.com>
2020-02-06 17:54:51 +0200

    add images content-length

--- src/news/image-handler.ts
+++ src/news/image-handler.ts
@@ -1,52 +1,56 @@
 import { Request, Response, NextFunction } from "express";
 import {
-    ImageSizeName,
-    ImageFormatHelper,
-    getImageMasterSizeName,
-    getImageSizeByName,
-    ImageFormat,
+  ImageSizeName,
+  ImageFormatHelper,
+  getImageMasterSizeName,
+  getImageSizeByName,
+  ImageFormat
 } from "@ournet/images-domain";
-import * as sharp from 'sharp';
-import * as got from 'got';
+import * as sharp from "sharp";
+import * as got from "got";
 import { Duplex } from "stream";
 
 const masterSizeName = getImageMasterSizeName();
 
 export default (req: Request, res: Response, next: NextFunction) => {
-    const id = req.params.id as string;
-    const format = req.params.format as ImageFormat;
-    const size = req.params.size as ImageSizeName;
+  const id = req.params.id as string;
+  const format = req.params.format as ImageFormat;
+  const size = req.params.size as ImageSizeName;
 
-    const originalFormat = ImageFormatHelper.getFormatById(id);
+  const originalFormat = ImageFormatHelper.getFormatById(id);
 
-    const url = `http://s3.eu-central-1.amazonaws.com/news.ournetcdn.net/${req.params.folder}/${req.params.prefix}/${masterSizeName}/${id}.${originalFormat}`;
+  const url = `http://s3.eu-central-1.amazonaws.com/news.ournetcdn.net/${req.params.folder}/${req.params.prefix}/${masterSizeName}/${id}.${originalFormat}`;
 
-    const stream = got
-        .stream(url, { timeout: 3000 })
-        .on('error', error => next(error));
+  const stream = got
+    .stream(url, { timeout: 3000 })
+    .on("error", (error: any) => next(error));
 
-    if (format === originalFormat && size === masterSizeName) {
-        return sendImage(stream, res, format);
+  if (format === originalFormat && size === masterSizeName) {
+    return sendImage(stream, res, format);
+  }
+  let instance = sharp();
+  if (format !== originalFormat) {
+    instance = instance.toFormat(format);
+  }
+  if (size !== masterSizeName) {
+    const newSize = getImageSizeByName(size);
+    if (size === "square") {
+      instance = instance.resize(newSize, newSize);
+    } else {
+      instance = instance.resize(newSize);
     }
-    let instance = sharp();
-    if (format !== originalFormat) {
-        instance = instance.toFormat(format);
-    }
-    if (size !== masterSizeName) {
-        const newSize = getImageSizeByName(size);
-        if (size === 'square') {
-            instance = instance.resize(newSize, newSize);
-        } else {
-            instance = instance.resize(newSize);
-        }
-    }
+  }
 
-    return sendImage(stream.pipe(instance), res, format);
-}
+  return sendImage(stream.pipe(instance), res, format);
+};
 
 function sendImage(stream: Duplex, res: Response, format: ImageFormat) {
-    res.setHeader('Content-Type', ImageFormatHelper.getMimeByFormat(format));
-    res.setHeader('Cache-Control', 'public, max-age=2592000'); // 30 days
-    
-    stream.pipe(res);
+  res.setHeader("content-type", ImageFormatHelper.getMimeByFormat(format));
+  res.setHeader("cache-control", "public,max-age=5184000"); // 60 days
+
+  stream.on("data", chunk => {
+    res.setHeader("content-length", chunk.length);
+  });
+
+  stream.pipe(res, { end: true });
 }
--- src/quotes/image-handler.ts
+++ src/quotes/image-handler.ts
@@ -5,36 +5,42 @@ import { Quote, QuoteStringFields } from "@ournet/api-client";
 import { Stream } from "stream";
 import { notFound } from "boom";
 
-
 export default async (req: Request, res: Response, next: NextFunction) => {
-    const id = req.params.id as string;
-    const format = req.params.format as ImageFormat;
+  const id = req.params.id as string;
+  const format = req.params.format as ImageFormat;
 
-    const query = ournetAPI.query<{ quote: Quote }>();
+  const query = ournetAPI.query<{ quote: Quote }>();
 
-    query.quotesQuoteById('quote', { fields: QuoteStringFields }, { id });
+  query.quotesQuoteById("quote", { fields: QuoteStringFields }, { id });
 
-    try {
-        const data = await ournetAPI.execure(query);
-        if (!data.quote) {
-            return next(notFound(`Not fount quote id=${id}`));
-        }
-        return sendImage(generateImage(data.quote, format), res, format);
-    } catch (e) {
-        next(e);
+  try {
+    const data = await ournetAPI.execure(query);
+    if (!data.quote) {
+      return next(notFound(`Not fount quote id=${id}`));
     }
-}
+    return sendImage(generateImage(data.quote, format), res, format);
+  } catch (e) {
+    next(e);
+  }
+};
 
 function sendImage(stream: Stream, res: Response, format: ImageFormat) {
-    res.setHeader('Content-Type', getContentType(format));
-    res.setHeader('Cache-Control', 'public, max-age=2592000'); // 30 days
-    stream.pipe(res)
+  res.setHeader("content-type", getContentType(format));
+  res.setHeader("cache-control", "public, max-age=5184000"); // 60 days
+
+  stream.on("data", chunk => {
+    res.setHeader("content-length", chunk.length);
+  });
+
+  stream.pipe(res, { end: true });
 }
 
 function getContentType(format: ImageFormat) {
-    switch (format) {
-        case 'png': return 'image/png';
-        case 'jpg': return 'image/jpeg';
-    }
-    throw new Error(`Invalid image format! ${format}`);
+  switch (format) {
+    case "png":
+      return "image/png";
+    case "jpg":
+      return "image/jpeg";
+  }
+  throw new Error(`Invalid image format! ${format}`);
 }
