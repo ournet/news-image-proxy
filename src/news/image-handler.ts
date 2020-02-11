e96f8316ae9b4011f53e692ad5da91f360c23cef
Dumitru C <canteadumitru@gmail.com>
2020-02-11 20:58:04 +0200

    use http instead of got

--- src/news/image-handler.ts
+++ src/news/image-handler.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import * as http from "http";
 import {
   ImageSizeName,
   ImageFormatHelper,
@@ -7,23 +8,46 @@ import {
   ImageFormat
 } from "@ournet/images-domain";
 import * as sharp from "sharp";
-import * as got from "got";
 import { Duplex } from "stream";
 
+const CACHE_CONTROL_VALUE = "public, max-age=5184000";
 const masterSizeName = getImageMasterSizeName();
 
-export default (req: Request, res: Response, next: NextFunction) => {
-  const id = req.params.id as string;
-  const format = req.params.format as ImageFormat;
-  const size = req.params.size as ImageSizeName;
+const sendImage = (stream: Duplex, res: Response, format: ImageFormat) => {
+  res.setHeader("content-type", ImageFormatHelper.getMimeByFormat(format));
+  res.setHeader("cache-control", CACHE_CONTROL_VALUE); // 60 days
+  let length = 0;
+  stream.on("data", chunk => {
+    length += chunk.length;
+    res.setHeader("content-length", length);
+  });
 
-  const originalFormat = ImageFormatHelper.getFormatById(id);
+  stream.pipe(res);
+};
 
-  const url = `http://s3.eu-central-1.amazonaws.com/news.ournetcdn.net/${req.params.folder}/${req.params.prefix}/${masterSizeName}/${id}.${originalFormat}`;
+const headersToDelete = [
+  "x-amz-id-2",
+  "accept-ranges",
+  "etag",
+  "server",
+  "x-amz-request-id",
+  "connection",
+  "last-modified"
+];
 
-  const stream = got
-    .stream(url, { timeout: 3000 })
-    .on("error", (error: any) => next(error));
+const handleResponse = (
+  originalFormat: ImageFormat,
+  format: ImageFormat,
+  size: ImageSizeName,
+  res: Response
+) => (response: http.IncomingMessage) => {
+  headersToDelete.forEach(header => delete response.headers[header]);
+
+  if (format === originalFormat && size === masterSizeName) {
+    response.headers["cache-control"] = CACHE_CONTROL_VALUE;
+    res.writeHead(response.statusCode || 200, response.headers);
+    return response.pipe(res);
+  }
 
   let instance = sharp();
   if (format !== originalFormat) {
@@ -38,21 +62,21 @@ export default (req: Request, res: Response, next: NextFunction) => {
     }
   }
 
-  return sendImage(stream.pipe(instance), res, format);
+  return sendImage(response.pipe(instance), res, format);
 };
 
-function sendImage(stream: Duplex, res: Response, format: ImageFormat) {
-  res.setHeader("Content-Type", ImageFormatHelper.getMimeByFormat(format));
-  res.setHeader("Cache-Control", "public, max-age=5184000"); // 60 days
-  let length = 0;
-  stream.on("data", chunk => {
-    length += chunk.length;
-    res.setHeader("content-length", length);
-  });
+export default (req: Request, res: Response, next: NextFunction) => {
+  const id = req.params.id as string;
+  const format = req.params.format as ImageFormat;
+  const size = req.params.size as ImageSizeName;
 
-  res.removeHeader("accept-ranges");
-  res.removeHeader("etag");
-  res.removeHeader("server");
+  const originalFormat = ImageFormatHelper.getFormatById(id);
 
-  stream.pipe(res);
-}
+  const url = `http://s3.eu-central-1.amazonaws.com/news.ournetcdn.net/${req.params.folder}/${req.params.prefix}/${masterSizeName}/${id}.${originalFormat}`;
+
+  const options = { timeout: 3000, method: "GET" };
+
+  http
+    .get(url, options, handleResponse(originalFormat, format, size, res))
+    .on("error", (error: any) => next(error));
+};
